5872e57de0f34cacc4831690e34bcfb584972fd5
Phan Van Ven <contact.phanvanven@gmail.com>
2024-06-04 04:45:20 +0700

    New Features: consumerToQueueNormal and consumerToQueueFailed in consumerQueue service. Update: server.js file

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 'use strict';
 
-const { consumerToQueue } = require('./src/services/consumerQueue.service');
+const { consumerToQueue, consumerToQueueNormal, consumerToQueueFailed } = require('./src/services/consumerQueue.service');
 const queueName = 'test-topic';
-consumerToQueue(queueName).then( () =>{
-    console.log(`Message consumer started ${queueName}`);
+
+// consumerToQueue(queueName).then( () =>{
+//     console.log(`Message consumer started ${queueName}`);
+// }).catch(err =>{
+//     console.log(`Message Error: ${err.message}`);
+// });
+
+consumerToQueueNormal(queueName).then( () =>{
+    console.log(`Message consumerToQueueNormal started`);
+}).catch(err =>{
+    console.log(`Message Error: ${err.message}`);
+});
+
+consumerToQueueFailed(queueName).then( () =>{
+    console.log(`Message consumerToQueueFailed started`);
 }).catch(err =>{
     console.log(`Message Error: ${err.message}`);
 });
--- src/services/consumerQueue.service.js
+++ src/services/consumerQueue.service.js
@@ -13,6 +13,50 @@ const messageService = {
         } catch (error) {
             console.log('Error consumerToQueue: ', error);
         }
+    },
+
+    // proccessing case
+    consumerToQueueNormal: async(nameQueue)=>{
+        try {
+            const {channel, connection} = await connectToRabbitMQ();
+            const notificationQueue = 'notificationQueueProcess';
+            const timeExpired = 15000;
+            setTimeout(()=>{
+                channel.consume(notificationQueue, msg =>{
+                    console.log(`Notification sent successfully: ${msg.content.toString()}`);
+                    channel.ack(msg);
+                })
+            }, timeExpired);
+        } catch (error) {
+            console.error(error);
+        }
+    },
+
+    // failure case
+    consumerToQueueFailed: async(nameQueue)=>{
+        try {
+            const {channel, connection} = await connectToRabbitMQ();
+            const notificationExchangeDLX = 'notificationExDLX'; // direct exchange (failure case)
+            const notificationRoutingKeyDLX = 'notificationRoutingKeyDLX'; // assert
+            const notificationHandler = 'notificationQueueHotFix';
+            
+            await channel.assertExchange(notificationExchangeDLX, 'direct',{
+                durable: true,
+            });
+
+            const queueResult = await channel.assertQueue(notificationHandler, {
+                exclusive: false
+            });
+
+            await channel.bindQueue(queueResult.queue, notificationExchangeDLX, notificationRoutingKeyDLX);
+            await channel.consume(queueResult.queue, msgFailed =>{
+                console.log(`this connection error, pls hot fix: ${msgFailed.content.toString()}`);
+            },{
+                noAck: true
+            })
+        } catch (error) {
+            console.error(error)
+        }
     }
 }
 
